361340947a563f4ea0bc204ee7ff3c86dd8383f2
Gutcal <70690295+Gutcal@users.noreply.github.com>
2020-12-10 16:28:54 +0200

    Add files via upload

---
+++ pr 3.py
@@ -0,0 +1,5 @@
+a=int(input("cati ani;"))
+l=a*12
+z=a*365
+o=(a*z)*24
+print("luni", l, "zile",z, "ore",o )
---
+++ problema 1.py
@@ -0,0 +1,5 @@
+x=int(input("dati nr total de pasari:"))
+g=x//2
+r=g//4
+g1=x-g-r
+print("gaini" ,g, "rate",r,"gasti", g1)
